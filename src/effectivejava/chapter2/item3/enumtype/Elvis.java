ef3577877ece057cde7088cb3d5751a2109af4a9
Delf-Lee <ttjs2008@naver.com>
2019-01-16 22:58:47 +0900

    Update item4, item5

---
+++ src/effectivejava/chapter2/item3/enumtype/Elvis.java
@@ -0,0 +1,36 @@
+package effectivejava.chapter2.item3.enumtype;
+
+// 열거 타입 방식의 싱글턴 - 바람직한 방법 (25쪽)
+public enum Elvis {
+    INSTANCE(0, 180),
+    I2(123, 123);
+
+    private int age;
+    private int height;
+
+    Elvis(int age, int height){
+        this.age = age;
+        this.height = height;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void leaveTheBuilding() {
+        System.out.println("기다려 자기야, 지금 나갈께!");
+    }
+
+    // 이 메서드는 보통 클래스 바깥(다른 클래스)에 작성해야 한다!
+    public static void main(String[] args) {
+        Elvis elvis = Elvis.INSTANCE;
+        System.out.println(elvis.getAge());
+        elvis.setAge(100);
+        System.out.println(elvis.getAge());
+        elvis.leaveTheBuilding();
+    }
+}
